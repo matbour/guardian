40269010ed6778740323c3a975035d2d60b9b8f5
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-04-22 18:27:54 +0200

    docs(contributing): add contributing page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   base: '/guardian/',
   title: 'Guardian',
-  description: 'Just playing around',
+  description: 'Highly configurable JSON Web Token implementation for Laravel and Lumen.',
   markdown: {
     lineNumbers: true
   },
@@ -10,6 +10,7 @@ module.exports = {
       { text: 'Configuration', link: '/configuration' },
       { text: 'Reference', link: '/reference' },
       { text: 'Case studies', link: '/case-studies' },
+      { text: 'Contributing', link: '/contributing' },
       { text: 'GitHub', link: 'https://github.com/mathieu-bour/guardian', target: '_blank' }
     ]
   }
